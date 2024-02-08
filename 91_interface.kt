5c1875bd364e82b79c838ea254f36f2367f5bc35
Seoyeon Kwon <syeon0204@gmail.com>
2024-02-08 16:51:21 +0900

    Create 91_interface.kt

---
+++ 91_interface.kt
@@ -0,0 +1,28 @@
+/*
+ * interface에 대하여 다룹니다.
+ */
+ 
+
+fun main(args: Array<String>) {
+	val item = Item("BOOK", 10_000)
+	println("Item name is ${item.name}, price is ${item.price}")
+	item.buy()
+	item.sell()
+}
+
+class Item(
+    val name: String,
+    val price: Int
+) : ItemTrade {
+    override fun buy() {
+	    println("[buy] $name")
+	}
+    override fun sell() {
+	    println("[sell] $name")
+	}
+}
+
+interface ItemTrade {
+	fun buy()
+	fun sell()
+}
